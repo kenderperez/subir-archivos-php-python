ca22c7fd125777c51d540e9584c0b00fab5b279d
kenderperez <kenderperez1995@gmail.com>
2020-05-05 13:02:19 -0400

    Update sube.php

--- sube.php
+++ sube.php
@@ -5,7 +5,7 @@ $nombre=$_FILES['archivo']['name'];
 $guardado=$_FILES['archivo']['tmp_name'];
 
 if(!file_exists('archivos')){
-	mkdir('archivos',777,true);
+	mkdir('archivos',0777,true);
 	if(file_exists('archivos')){
 		if(move_uploaded_file($guardado, 'archivos/'.$nombre)){
 			echo shell_exec('python3 app.py'.' '.$nombre);
